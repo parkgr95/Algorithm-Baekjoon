e9627196a645175c2093f2915b32aeadbce99fe9
82106 <pkr951113@gmail.com>
2021-07-01 15:25:56 +0900

    0630

---
+++ SW 역량 테스트 준비 - 기초(브루트 포스 (N과 M))/15664.py
@@ -0,0 +1,23 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+array = list(map(int, sys.stdin.readline().split()))
+array.sort()
+
+visited = [False] * n
+answer = []
+
+def dfs(depth, index):
+    if depth == m:
+        print(" ".join(map(str, answer)))
+        return
+    last = 0
+    for i in range(index, n):
+        if not visited[i] and last != array[i]:
+            visited[i] = True
+            answer.append(array[i])
+            last = array[i]
+            dfs(depth + 1, i + 1)
+            answer.pop()
+            visited[i] = False
+dfs(0, 0)
