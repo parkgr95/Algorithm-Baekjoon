7326c0c88bd992b2dd5eed10bb587c7a1a188530
82106 <pkr951113@gmail.com>
2021-08-10 01:24:26 +0900

    0809

--- SW 역량 테스트 준비 - 기초(다이나믹 프로그래밍)/9465.py
+++ SW 역량 테스트 준비 - 기초(다이나믹 프로그래밍)/9465.py
@@ -1,17 +1,19 @@
 import sys
 
-t = int(sys.stdin.readline())
-for _ in range(t):
-    n = int(sys.stdin.readline())
+if __name__ == "__main__":
+    for _ in range(int(sys.stdin.readline().rstrip())):
+        n = int(sys.stdin.readline().rstrip())
 
-    array = []
-    for i in range(2):
-        array.append(list(map(int, sys.stdin.readline().split())))
+        array = [[0]*100000, [0]*100000]
+        for idx, var in enumerate(map(int, sys.stdin.readline().split())):
+            array[0][idx] = var
+        for idx, var in enumerate(map(int, sys.stdin.readline().split())):
+            array[1][idx] = var
 
-    array[0][1] += array[1][0]
-    array[1][1] += array[0][0]
-    for i in range(2, n):
-        array[0][i] += max(array[1][i - 1], array[1][i - 2])
-        array[1][i] += max(array[0][i - 1], array[0][i - 2])
+        array[0][1] += array[1][0]
+        array[1][1] += array[0][0]
+        for i in range(2, n):
+            array[0][i] += max(array[1][i - 1], array[1][i - 2])
+            array[1][i] += max(array[0][i - 1], array[0][i - 2])
 
-    print(max(array[0][n - 1], array[1][n - 1]))
+        print(max(array[0][n - 1], array[1][n - 1]))
