87c4784e32bbcc07020bc65cf361e775ac14d6bc
park95gr <pkr951113@gmail.com>
2022-04-22 23:50:57 +0900

    0422

--- 단기간 성장/12865.py
+++ 단기간 성장/12865.py
@@ -11,7 +11,7 @@ dp = [[0] * (k + 1) for _ in range(n + 1)]
 # print(dp)
 
 for i in range(1, n + 1):
-    for j in range(1, k + 1):
+    for j in range(1, k + 1): # 가방의 용량
         w, v = bp[i][0], bp[i][1]
 
         if j < w:  # 현재 허용 무게보다 물건의 무게가 더 무겁다면 넣지 않는다.
