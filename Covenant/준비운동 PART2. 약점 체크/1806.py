4f5d5b67ed583557df73244cf6504cdf1bf01e8c
park95gr <pkr951113@gmail.com>
2022-05-25 01:53:16 +0900

    0524

--- Covenant/준비운동 PART2. 약점 체크/1806.py
+++ Covenant/준비운동 PART2. 약점 체크/1806.py
@@ -1,29 +1,29 @@
+"""
+day : 0524
+num : 1806
+idea : 투 포인터, 부분합
+ref :
+"""
+
 n, s = map(int, input().split())
-array = list(map(int, input().split()))
+tmp = list(map(int, input().split()))
 
 # 접두사 합(Prefix Sum) 배열 계산
-sum_value = 0
-prefix_sum = [0]
-for a in array:
-    sum_value += a
-    prefix_sum.append(sum_value)
+arr = [0]
+for t in tmp:
+    arr.append(t + arr[-1])
 
-answer = 1000000001
-end = 1
+ans = 1e9
+end = 0
 # start를 차례대로 증가시키며 반복
 for start in range(n):
     # end를 가능한 만큼 이동시키기
     # 구간 합 계산
-    while prefix_sum[end] - prefix_sum[start] < s and end < n:
+    while arr[end] - arr[start] < s and end < n:
         end += 1
     # 부분합이 s이상일 때, 길이가 짧으면 바꿔준다.
-    if prefix_sum[end] - prefix_sum[start] >= s:
-        if end - start < answer:
-            answer = end - start
+    if arr[end] - arr[start] >= s:
+        if end - start < ans:
+            ans = end - start
 
-if answer != 1000000001:
-    print(answer)
-else:
-    print(0)
-
-# 구간합 & 부분합
+print(ans if ans != 1e9 else 0)
